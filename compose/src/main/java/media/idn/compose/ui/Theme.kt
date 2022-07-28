044eead5d4e911b094bbab4d367c95dd9835390f
Muhammad Zuhriyan Sauqi <hello@zuhriyansauqi.com>
2022-07-28 10:31:32 +0700

    Removed accompanist SystemUIController

--- compose/src/main/java/media/idn/compose/ui/Theme.kt
+++ compose/src/main/java/media/idn/compose/ui/Theme.kt
@@ -2,25 +2,9 @@ package media.idn.compose.ui
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 @Composable
 fun IDNTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-// TODO: CRASH!!
-//region systemUiController
-//    val systemUiController = rememberSystemUiController()
-//    val useDarkIcons = IDNTheme.colors.isLight
-//
-//    SideEffect {
-//        systemUiController.setSystemBarsColor(
-//            color = Color.Transparent,
-//            darkIcons = useDarkIcons,
-//        )
-//    }
-//endregion
-
     val colors = if (darkTheme) DarkThemeColors else LightThemeColors
     val typography = DefaultTypography
     val shapes = DefaultShapes
